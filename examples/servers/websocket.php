73ee0069b1e83b473642c8232b2d5141cde0071a
Demin Yin <deminy@deminy.net>
2019-09-21 00:41:32 -0700

    update comments

--- examples/servers/websocket.php
+++ examples/servers/websocket.php
@@ -1,9 +1,11 @@
 #!/usr/bin/env php
 <?php
 /**
- *
+ * In this example we start a WebSocket server on port 9503. Please check next section to see how to test the example.
+ */
 
-In this example we start a WebSocket server on port 9503.
+$comments = <<<EOT
+This dummy variable is only for documenting purpose.
 
 If you hit that port with command "curl -i http://127.0.0.1:9503" (using the HTTP/1 protocol), you will get an HTTP 400
 response (Bad Request).
@@ -31,16 +33,12 @@ If the WebSocket server runs properly, you should see following response:
     Sec-WebSocket-Accept: qGEgH3En71di5rrssAZTmtRTyFk=
     Sec-WebSocket-Version: 13
     Server: swoole-http-server
+EOT;
 
- */
-
-use Swoole\WebSocket\Server;
-use Swoole\WebSocket\Frame;
-
-$server = new Server("0.0.0.0", 9503, SWOOLE_BASE);
+$server = new Swoole\WebSocket\Server("0.0.0.0", 9503, SWOOLE_BASE);
 $server->on(
     "message",
-    function (Server $server, Frame $frame) {
+    function (Swoole\WebSocket\Server $server, Swoole\WebSocket\Frame $frame) {
         $server->push($frame->fd, "Hello, {$frame->data}");
     }
 );
