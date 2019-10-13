c4ef10a184369abd81fbee32af78e9bf5be2b4d6
Demin Yin <deminy@swoole.com>
2019-10-13 15:14:44 -0700

    add new example for the HTTP/1 client

--- README.md
+++ README.md
@@ -62,3 +62,13 @@ Docker container.
             * support multiple protocols on same server
         * multiple ports listening
 * client-side programming
+    * [HTTP/1 client](https://github.com/deminy/swoole-by-examples/blob/master/examples/servers/http1.php)
+    * HTTP/2 client
+    * WebSocket client
+    * TCP client
+* Swoole extensions
+    * async
+    * orm
+    * postgresql
+    * serialize
+    * zookeeper
---
+++ examples/clients/http1.php
@@ -0,0 +1,50 @@
+#!/usr/bin/env php
+<?php
+/**
+ * In this example two HTTP/1 requests are made in non-blocking mode. Since the 2nd request usually takes
+ * much less time to finish, you should see following output in most cases:
+ *     Done executing the second HTTP/1 request.
+ *     Done executing the first HTTP/1 request.
+ *
+ * How to run this script:
+ *     docker exec -t  $(docker ps -qf "name=client") bash -c "./clients/http1.php"
+ */
+
+use Swoole\Coroutine\Http\Client;
+
+co\run(
+    function () {
+        // For the 1st one, we make an HTTPS request to download an image file from GitHub. Once this script
+        // finishes execution, you should see an image file "mascot.png" under same directory of this script.
+        go(function () {
+            $client = new Client("raw.githubusercontent.com", 443, true);
+            $client->set(
+                [
+                    "timeout" => -1,
+                ]
+            );
+            $client->setHeaders(
+                [
+                    "Accept-Encoding" => "gzip",
+                ]
+            );
+            $client->download("/swoole/swoole-src/master/mascot.png", __DIR__ . "/mascot.png");
+
+            echo "Done executing the first HTTP/1 request.\n";
+        });
+
+        // For the 2nd one, we make an HTTP request to the HTTP/1 server started in the "server" container.
+        go(function () {
+            $client = new Client("server", 9501);
+            $client->get("/");
+
+            // You can uncomment following code to print out the HTTP response headers and body.
+            //foreach ($client->getHeaders() as $key => $value) {
+            //    echo "{$key}: $value\n";
+            //}
+            //echo "\n" . $client->body;
+
+            echo "Done executing the second HTTP/1 request.\n";
+        });
+    }
+);
