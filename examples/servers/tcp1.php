e597ba98c4478cf448cb0dfa04f1dac32fe606c1
Demin Yin <deminy@deminy.net>
2021-07-13 11:18:04 -0700

    update doc
    Signed-off-by: Demin Yin <deminy@deminy.net>

--- examples/servers/tcp1.php
+++ examples/servers/tcp1.php
@@ -2,20 +2,16 @@
 <?php
 
 /**
- * In this example we start a TCP server in event-driven style on port 9505. Please check next section to see how to
- * test the example.
+ * In this example we start a TCP server in event-driven style on port 9505.
+ *
+ * To test the TCP server, you can execute a netcat command in the client container to talk to the TCP server:
+ *     docker exec -ti $(docker ps -qf "name=client") nc server 9505
+ *
+ * Now you can start typing something and hit the return button to send it to the TCP server. Whatever you type, the TCP
+ * server echos it back.
+ *
+ * Once done, you can press CTRL+C to stop.
  */
-
-$comments = <<<EOT
-This dummy variable is only for documenting purpose.
-
-To test the TCP server, you can create a new Bash session in the client container first using following command:
-    docker exec -ti $(docker ps -qf "name=client") bash
-Then execute a netcat command in the Bash session to talk to the TCP server:
-    nc server 9505
-Now you can start typing something and hit the return button to send it to the TCP server. Whatever you type, the TCP
-server echos it back.
-EOT;
 
 use Swoole\Server;
 
