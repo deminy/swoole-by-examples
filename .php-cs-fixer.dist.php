3aeab590eeea174e01b1d90fb33ad1b4b91e027b
Demin Yin <deminy@deminy.net>
2021-10-06 11:39:55 -0700

    coding style updates
    Signed-off-by: Demin Yin <deminy@deminy.net>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,15 +2,16 @@
 
 declare(strict_types=1);
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@DoctrineAnnotation'                    => true,
         '@PhpCsFixer'                            => true,
         '@PSR2'                                  => true,
         '@Symfony'                               => true,
+        'align_multiline_comment'                => ['comment_type' => 'all_multiline'],
         'array_syntax'                           => ['syntax' => 'short'],
-        'binary_operator_spaces'                 => ['align_double_arrow' => true, 'align_equals' => true],
+        'binary_operator_spaces'                 => ['operators' => ['=' => 'align', '=>' => 'align', ]],
         'blank_line_after_namespace'             => true,
         'blank_line_before_statement'            => ['statements' => ['declare']],
         'class_attributes_separation'            => true,
@@ -19,7 +20,7 @@ return PhpCsFixer\Config::create()
         'combine_consecutive_unsets'             => true,
         'declare_strict_types'                   => true,
         'general_phpdoc_annotation_remove'       => ['annotations' => ['author']],
-        'header_comment'                         => ['commentType' => 'PHPDoc', 'header' => '', 'separate' => 'bottom', 'location' => 'after_open'],
+        'header_comment'                         => ['comment_type' => 'PHPDoc', 'header' => '', 'separate' => 'bottom', 'location' => 'after_open'],
         'increment_style'                        => ['style' => 'post'],
         'lambda_not_used_import'                 => false,
         'linebreak_after_opening_tag'            => true,
@@ -29,11 +30,13 @@ return PhpCsFixer\Config::create()
         'multiline_whitespace_before_semicolons' => ['strategy' => 'new_line_for_chained_calls'],
         'no_unused_imports'                      => true,
         'no_useless_else'                        => true,
+        'no_useless_return'                      => true,
         'not_operator_with_space'                => false,
         'not_operator_with_successor_space'      => false,
         'php_unit_strict'                        => false,
         'phpdoc_align'                           => ['align' => 'left'],
-        'phpdoc_no_empty_return'                 => true,
+        'phpdoc_no_empty_return'                 => false,
+        'phpdoc_types_order'                     => ['sort_algorithm' => 'none', 'null_adjustment' => 'always_last'],
         'phpdoc_separation'                      => false,
         'phpdoc_summary'                         => false,
         'ordered_class_elements'                 => true,
