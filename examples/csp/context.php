6096208c33af05e877bc84e4b7bebb9b5173c17d
Demin Yin <deminy@deminy.net>
2024-12-04 16:16:04 -0800

    stop using dynamic properties in the Context object
    Signed-off-by: Demin Yin <deminy@deminy.net>

--- examples/csp/context.php
+++ examples/csp/context.php
@@ -18,16 +18,14 @@ declare(strict_types=1);
  *     docker compose exec -t client bash -c "./csp/context.php"
  *
  * The output should look like the following:
- *     Name of the 1st object in the Context object of coroutine #1: co1_obj1
- *     Name of the 2nd object in the Context object of coroutine #2: co2_obj2
+ *     Name of the object in the Context object of coroutine #1: co1_obj
+ *     Name of the object in the Context object of coroutine #2: co2_obj
  *
  *     Coroutine #2 is exiting. The context object of it will be destroyed.
- *     Object "co2_obj2" is destroyed.
- *     Object "co2_obj1" is destroyed.
+ *     Object "co2_obj" is destroyed.
  *
  *     Coroutine #1 is exiting. The context object of it will be destroyed.
- *     Object "co1_obj2" is destroyed.
- *     Object "co1_obj1" is destroyed.
+ *     Object "co1_obj" is destroyed.
  */
 
 use Swoole\Coroutine;
@@ -48,19 +46,22 @@ run(function () {
         }
     };
 
+    // Prior to PHP 8.2, items in the Context object of a coroutine could be set or accessed using dynamic properties,
+    // e.g.,
+    //   Coroutine::getContext($cid)->foo = 'bar';
+    // Starting with PHP 8.2, dynamic properties are deprecated, so we use array-style access instead.
+
     // The Context object of a coroutine works the same as an \ArrayObject object.
-    Coroutine::getContext()['co1_obj1']   = new $class('co1_obj1'); // @phpstan-ignore offsetAccess.nonOffsetAccessible
-    $cid                                  = Coroutine::getCid();    // Coroutine::getCid() returns 1.
-    Coroutine::getContext($cid)->co1_obj2 = new $class('co1_obj2'); // @phpstan-ignore property.nonObject
+    Coroutine::getContext()['co1_obj'] = new $class('co1_obj'); // @phpstan-ignore offsetAccess.nonOffsetAccessible
+    $cid                               = Coroutine::getCid();   // Coroutine::getCid() returns 1.
 
     Coroutine::create(function () use ($class) {
         // The Context object of a coroutine works the same as an \ArrayObject object.
-        Coroutine::getContext()['co2_obj1']   = new $class('co2_obj1'); // @phpstan-ignore offsetAccess.nonOffsetAccessible
-        $cid                                  = Coroutine::getCid();    // Coroutine::getCid() returns 2.
-        Coroutine::getContext($cid)->co2_obj2 = new $class('co2_obj2'); // @phpstan-ignore property.nonObject
+        Coroutine::getContext()['co2_obj'] = new $class('co2_obj'); // @phpstan-ignore offsetAccess.nonOffsetAccessible
+        $cid                               = Coroutine::getCid();   // Coroutine::getCid() returns 2.
 
-        echo 'Name of the 1st object in the Context object of coroutine #1: ', Coroutine::getContext(1)['co1_obj1']->name, PHP_EOL; // @phpstan-ignore-line
-        echo 'Name of the 2nd object in the Context object of coroutine #2: ', Coroutine::getContext(2)->co2_obj2->name, PHP_EOL; // @phpstan-ignore property.nonObject
+        echo 'Name of the object in the Context object of coroutine #1: ', Coroutine::getContext(1)['co1_obj']->name, PHP_EOL; // @phpstan-ignore-line
+        echo 'Name of the object in the Context object of coroutine #2: ', Coroutine::getContext(2)['co2_obj']->name, PHP_EOL; // @phpstan-ignore property.nonObject
 
         echo PHP_EOL, 'Coroutine #2 is exiting. The context object of it will be destroyed.', PHP_EOL;
     });
